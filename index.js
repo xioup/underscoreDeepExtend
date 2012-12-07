88a8ceab0a4f3de4d40095559b48258928e0324d
pygy <pygy79@gmail.com>
2012-12-07 05:12:46 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ *   
+ *   This program is free software: you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *   
+ *   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *   
+ *   You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+**/
+
+
+( // Module boilerplate to support browser globals, node.js and AMD.
+  (typeof module !== "undefined" && function (m) { module.exports = m(); }) ||
+  (typeof define === "function" && function (m) { define("brozula", ['underscore'], m); }) ||
+  (function (m) { window.brozula = m(); })
+)(function () { return function(_) {
+
+return function deepExtend (obj) {
+  var parentRE = /#{\s*?_\s*?}/,
+      slice = Array.prototype.slice,
+      hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  _.each(slice.call(arguments, 1), function(source) {
+    for (var prop in source) {
+      if (hasOwnProperty.call(source, prop)) {
+        if (_.isUndefined(obj[prop])) {
+          obj[prop] = source[prop];
+        }
+        else if (_.isString(source[prop]) && parentRE.test(source[prop])) {
+          if (_.isString(obj[prop])) {
+            obj[prop] = source[prop].replace(parentRE, obj[prop]);
+          }
+        }
+        else if (_.isArray(obj[prop]) || _.isArray(source[prop])){
+          if (!_.isArray(obj[prop]) || !_.isArray(source[prop])){
+            throw 'Error: Trying to combine an array with a non-array (' + prop + ')';
+          } else {
+            obj[prop] = _.reject(_.deepExtend(obj[prop], source[prop]), function (item) { return _.isNull(item);});
+          }
+        }
+        else if (_.isObject(obj[prop]) || _.isObject(source[prop])){
+          if (!_.isObject(obj[prop]) || !_.isObject(source[prop])){
+            throw 'Error: Trying to combine an object with a non-object (' + prop + ')';
+          } else {
+            obj[prop] = _.deepExtend(obj[prop], source[prop]);
+          }
+        } else {
+          obj[prop] = source[prop];
+        }
+      }
+    }
+  });
+  return obj;
+};
+
+};});
