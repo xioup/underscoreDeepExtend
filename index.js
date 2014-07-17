098948ee21c5c34e14f0e75add21dbe7af87c076
Pierre-Yves Gerardy <py@py.gy>
2014-07-17 23:09:19 +0200

    Add support for Date props.
    Kudos to Kurt Milam for the heads up.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ return function underscoreDeepExtend(obj) {
   _.each(slice.call(arguments, 1), function(source) {
     for (var prop in source) {
       if (hasOwnProperty.call(source, prop)) {
-        if (_.isUndefined(obj[prop]) || _.isFunction(obj[prop]) || _.isNull(source[prop])) {
+        if (_.isUndefined(obj[prop]) || _.isFunction(obj[prop]) || _.isNull(source[prop]) || _.isDate(source[prop])) {
           obj[prop] = source[prop];
         }
         else if (_.isString(source[prop]) && parentRE.test(source[prop])) {
