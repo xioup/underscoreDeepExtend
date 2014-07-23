92806ca26996f3534379e416152b5d3d9b0191c0
kurtmilam <kurt.milam@xioup.com>
2014-07-23 22:23:44 +0200

    Removed hasOwnProperty check & Error Objects
    1. Removed the hasOwnProperty check to be more consistent with
    underscore.js . See: https://github.com/pygy/underscoreDeepExtend/issues/6
    2. Changed the two Errors to throw new Error() rather than strings

--- index.js
+++ index.js
@@ -15,36 +15,33 @@
 
 return function underscoreDeepExtend(obj) {
   var parentRE = /#{\s*?_\s*?}/,
-  slice = Array.prototype.slice,
-  hasOwnProperty = Object.prototype.hasOwnProperty;
+  slice = Array.prototype.slice;
 
   _.each(slice.call(arguments, 1), function(source) {
     for (var prop in source) {
-      if (hasOwnProperty.call(source, prop)) {
-        if (_.isUndefined(obj[prop]) || _.isFunction(obj[prop]) || _.isNull(source[prop]) || _.isDate(source[prop])) {
-          obj[prop] = source[prop];
+      if (_.isUndefined(obj[prop]) || _.isFunction(obj[prop]) || _.isNull(source[prop]) || _.isDate(source[prop])) {
+        obj[prop] = source[prop];
+      }
+      else if (_.isString(source[prop]) && parentRE.test(source[prop])) {
+        if (_.isString(obj[prop])) {
+          obj[prop] = source[prop].replace(parentRE, obj[prop]);
         }
-        else if (_.isString(source[prop]) && parentRE.test(source[prop])) {
-          if (_.isString(obj[prop])) {
-            obj[prop] = source[prop].replace(parentRE, obj[prop]);
-          }
-        }
-        else if (_.isArray(obj[prop]) || _.isArray(source[prop])){
-          if (!_.isArray(obj[prop]) || !_.isArray(source[prop])){
-            throw 'Error: Trying to combine an array with a non-array (' + prop + ')';
-          } else {
-            obj[prop] = _.reject(_.deepExtend(obj[prop], source[prop]), function (item) { return _.isNull(item);});
-          }
-        }
-        else if (_.isObject(obj[prop]) || _.isObject(source[prop])){
-          if (!_.isObject(obj[prop]) || !_.isObject(source[prop])){
-            throw 'Error: Trying to combine an object with a non-object (' + prop + ')';
-          } else {
-            obj[prop] = _.deepExtend(obj[prop], source[prop]);
-          }
+      }
+      else if (_.isArray(obj[prop]) || _.isArray(source[prop])){
+        if (!_.isArray(obj[prop]) || !_.isArray(source[prop])){
+          throw new Error('Trying to combine an array with a non-array (' + prop + ')');
         } else {
-          obj[prop] = source[prop];
+          obj[prop] = _.reject(_.deepExtend(obj[prop], source[prop]), function (item) { return _.isNull(item);});
         }
+      }
+      else if (_.isObject(obj[prop]) || _.isObject(source[prop])){
+        if (!_.isObject(obj[prop]) || !_.isObject(source[prop])){
+          throw new Error('Trying to combine an object with a non-object (' + prop + ')');
+        } else {
+          obj[prop] = _.deepExtend(obj[prop], source[prop]);
+        }
+      } else {
+        obj[prop] = source[prop];
       }
     }
   });
